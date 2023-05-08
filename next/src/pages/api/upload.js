c8eb0e1f66ef40c3575776a9142eeac0472ed708
Dănilă Ana-Maria <anamaria1616d@gmail.com>
2023-05-08 11:26:57 +0300

    Code cleanup

--- next/src/pages/api/upload.js
+++ next/src/pages/api/upload.js
@@ -1,12 +1,17 @@
-// import ThirdwebStorage from "@thirdweb-dev/storage";
-// import fs from "fs";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
+import fs from "fs";
 
-// const storage = new ThirdwebStorage();
-// const photo = "../../assets/cat.jpeg";
-// (async () => {
-//   const upload = await storage.upload(fs.readFileSync(photo));
+export default function handler(req, res) {
+  const storage = new ThirdwebStorage();
+  const photo =
+    "/Users/anadanila/Documents/GitHub/Licenta-2023-v2/next/src/assets/cat.jpeg";
+  (async () => {
+    const upload = await storage.upload(fs.readFileSync(photo));
 
-//   console.log(storage.resolveScheme(upload));
-// })();
+    console.log(storage.resolveScheme(upload));
+  })();
 
-console.log("aici");
+  console.log("aici");
+
+  res.status(200).json({ name: "John Doe" });
+}
--- server/src/routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const generateSignature = require("../controllers/generate");
-
-const router = express.Router();
-
-router.post("/generate", generateSignature);
-
-module.exports = router;
