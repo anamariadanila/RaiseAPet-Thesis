417ae25d874dd5a5e3f2e7cd737270b2bddd85b2
Dănilă Ana-Maria <anamaria1616d@gmail.com>
2023-06-19 01:16:35 +0300

    Button style changed

--- next/src/components/ButtonConnect.jsx
+++ next/src/components/ButtonConnect.jsx
@@ -4,26 +4,26 @@ import Button from "@mui/material/Button";
 
 const ButtonConnect = ({ title, btnType, handleClick, img, style }) => {
   return (
-    <Stack spacing={2} direction="row">
-      <Button
-        variant="contained"
-        size="large"
-        sx={{
-          height: "4rem",
-          width: "8rem",
-          mt: "0.5rem",
-          borderRadius: "90px",
-          fontWeight: "bold",
-        }}
-        color="button"
-        type={btnType}
-        onClick={handleClick}
-        style={style}
-      >
-        {title}
-        {img && <img src={img} alt="image" width="30px" height="30px" />}
-      </Button>
-    </Stack>
+    // <Stack spacing={2} direction="row">
+    <Button
+      variant="contained"
+      size="medium"
+      sx={{
+        height: "4rem",
+        width: "7rem",
+        mt: "0.5rem",
+        borderRadius: "90px",
+        fontWeight: "bold",
+      }}
+      color="button"
+      type={btnType}
+      onClick={handleClick}
+      style={style}
+    >
+      {title}
+      {img && <img src={img} alt="image" width="30px" height="30px" />}
+    </Button>
+    // </Stack>
   );
 };
 
